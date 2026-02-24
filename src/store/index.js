8211b7a147ae39c13f35d46ddba825abf0a8ad7c
SQUADGOO ADMIN <admin@squadgoo.com.au>
2026-02-24 17:10:21 +1100

    Initialize Redux store with auth, user, and ui reducers

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+import userReducer from './userSlice';
+import uiReducer from './uiSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    user: userReducer,
+    ui: uiReducer
+  },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: false
+    })
+});
+
+export default store;
